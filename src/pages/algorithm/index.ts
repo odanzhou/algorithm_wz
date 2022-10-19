4e1df922ad8d9941ab7aaba929465efeb45f23c4
odanzhou <odanzhou@gmail.com>
2022-10-19 13:10:28 +0800

    Update index.ts

--- src/pages/algorithm/index.ts
+++ src/pages/algorithm/index.ts
@@ -10,3 +10,18 @@ const fn  = (n) => {
     }
     return res
 }
+
+// 优化，减少二次遍历的次数
+const fnGen2  = (n) => {
+    const res = Array.from({ length: n}, () => [])
+    res[0] = [1]
+    for(let i = 1; i < n; i++) {
+        for(let j = 0; j <= Math.floor(i / 2); j++) {
+            const val = (res[i - 1][j - 1] || 0) + (res[i - 1][j] || 0)
+            res[i][j] = val
+            res[i][i - j] = val
+            
+        }
+    }
+    return res
+}
